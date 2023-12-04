dff0b79f3d52848c2dfa39c169acd50798ccd13e
thupq <phamquangthu2@gmail.com>
2023-12-05 01:16:59 +0700

    update button

--- src/layouts/Button.jsx
+++ src/layouts/Button.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Button = (props) => {
   return (
     <div>
-      <button className="px-6 py-1 border-2 border-white bg-[#FFDCAB] hover:text-[#AB6B2E] transition-all rounded-full">
+      {/*<button className="px-6 py-1 border-2 border-white bg-[#FFDCAB] hover:text-[#AB6B2E] transition-all rounded-full">*/}
+        <button
+            className="bg-transparent hover:bg-[#FFDCAB] text-amber-950 font-semibold hover:text-white py-2 px-4
+            hover:border-amber-50
+            border border-amber-700 hover:border-transparent rounded">
         {props.title}
       </button>
     </div>
